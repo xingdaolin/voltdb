a48448101b6680b52e8b58c9423a7ecbef319145
Rui Shen <rshen@voltdb.com>
2017-07-21 16:03:16 -0400

    ENG-12827: Remove the DR replicated stream

--- tests/ee/executors/OptimizedProjectorTest.cpp
+++ tests/ee/executors/OptimizedProjectorTest.cpp
@@ -521,6 +521,7 @@ int main(int argc, char* argv[]) {
     boost::scoped_ptr<voltdb::Pool> testPool(new voltdb::Pool());
     voltdb::UndoQuantum* wantNoQuantum = NULL;
     voltdb::Topend* topless = NULL;
+    boost::scoped_ptr<voltdb::AbstractDRTupleStream> drStream(new voltdb::DRTupleStream(0, 1024));
     boost::scoped_ptr<voltdb::ExecutorContext>
         executorContext(new voltdb::ExecutorContext(0,              // siteId
                                                     0,              // partitionId
@@ -530,7 +531,7 @@ int main(int argc, char* argv[]) {
                                                     NULL,           // engine
                                                     "",             // hostname
                                                     0,              // hostId
-                                                    NULL,           // drTupleStream
+                                                    drStream.get(), // drTupleStream
                                                     NULL,           // drReplicatedStream
                                                     0));            // drClusterId
 
