e8361bcb6f59e5e7c275b5bd2a7db08b48b40ce1
Brian Shaw <bshaw@voltdb.com>
2017-07-27 14:03:34 -0400

    ENG-12902 address NPE on rejoin

--- src/frontend/org/voltdb/RealVoltDB.java
+++ src/frontend/org/voltdb/RealVoltDB.java
@@ -3895,7 +3895,11 @@ public class RealVoltDB implements VoltDBInterface, RestoreAgent.Callback, HostM
     @Override
     public OperationMode getStartMode()
     {
-        return m_startMode;
+        if (m_startMode == null) {
+            return OperationMode.RUNNING;
+        } else {
+            return m_startMode;
+        }
     }
 
     @Override
